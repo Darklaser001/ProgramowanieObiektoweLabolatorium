c20c139aff5c174833b7896d6e1e6d70fb1cce06
Jakub Tokarski <w68892@student.wsiz.edu.pl>
2023-10-19 15:49:17 +0200

    Lab2

---
+++ Lab2/Constructions/Construction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Constructions
+{
+    class Construction
+    {
+        public Construction(double height, double width, int entrances, int humanCapacity, string buildMaterial)
+        {
+            this.Height = height;
+            this.Width = width;
+            this.Entrances = entrances;
+            this.HumanCapacity = humanCapacity;
+            this.BuildMaterial = buildMaterial;
+        }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial { get; set; }
+    }
+}
+
+/*
+1. Height typu zmiennoprzecinkowego
+2. Width typu zmiennoprzecinkowego
+3. Entrances typu liczba całkowita
+4. HumanCapacity typu liczba całkowita
+5. BuildMaterial typu tekstowego
+*/
---
+++ Lab2/Program.cs
@@ -0,0 +1,15 @@
+﻿using Lab2.Constructions;
+using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var construction = new Construction(12, 34, 5, 6, "Stone");
+
+            Console.WriteLine(construction.BuildMaterial);
+        }
+    }
+}
